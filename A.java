3ec9f6fd20b032c6f205fe7971e2cbd49d65a2e2
Maywop <93273627+Maywop@users.noreply.github.com>
2024-04-18 18:53:55 +0200

    Update A.java

--- A.java
+++ A.java
@@ -1,11 +1,9 @@
 public class A {
-    // Declare and initialize the flag variable
     private boolean flag = false;
 
     public void abc() {}
 
     public void foo(A a) {
-        // Correctly check the value of the flag
         if (flag)
             return;
         a.abc();
